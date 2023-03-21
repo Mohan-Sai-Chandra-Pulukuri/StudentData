94ceca812522c00561275faad8b251b9cf450d51
MohanSaiChandraPulukuri <mohansaichandra@yahoo.com>
2023-03-21 16:33:20 +0530

    Changed the Main Server File

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const bodyParser = require('body-parser')
+const application = express()
+application.use(bodyParser.json())
+const studentRouter = require('./routes/StudentData')
+application.use('/student',studentRouter)
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+application.listen(9000,()=>{console.log("Server is Running on the Port 9000")})
